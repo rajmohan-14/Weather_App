54e7a0da4806c5d44f553f867df8c837a5c85fa6
rajmohan-14 <rajmohan14k@gmail.com>
2026-02-15 15:14:20 +0530

    enhanced user interface with fully working weather api

--- core/views.py
+++ core/views.py
@@ -9,21 +9,32 @@ def home(request):
     if request.method == "POST":
         city = request.POST.get('city')
 
-        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
-
+        url = f"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={API_KEY}&units=metric"
         response = requests.get(url)
         data = response.json()
 
-        if response.status_code == 200:
+        if response.status_code == 200 and "list" in data:
+
+            current = data["list"][0]
+
+            daily_forecast = []
+            for item in data["list"]:
+                if "12:00:00" in item["dt_txt"]:
+                    daily_forecast.append({
+                        "date": item["dt_txt"].split(" ")[0],
+                        "temp": item["main"]["temp"],
+                        "icon": item["weather"][0]["icon"],
+                    })
+
             context = {
                 "city": city,
-                "temperature": data["main"]["temp"],
-                "humidity": data["main"]["humidity"],
-                "description": data["weather"][0]["description"],
+                "temperature": current["main"]["temp"],
+                "description": current["weather"][0]["description"],
+                "icon": current["weather"][0]["icon"],
+                "forecast": daily_forecast[:5]
             }
+
         else:
-            context = {
-                "error": "City not found"
-            }
+            context["error"] = "City not found"
 
     return render(request, "core/home.html", context)
